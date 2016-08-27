75b7526c4d14a6a94d99d4a18f0bf8997e314a6b
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2016-08-27 14:12:35 +0200

    Changed default input to be for simple sphere case

--- source/inputs.py
+++ source/inputs.py
@@ -1,18 +1,15 @@
 
 class TrajectoryInputs:
     name = 'default'
-    step_count = 3
+    step_count = 5
     # @todo: Generalize the plot view window
-    plot_xlim = [-1., 1.]
-    plot_ylim = [-1.5, 1.5]
+    plot_xlim = [-1.5, 1.5]
+    plot_ylim = [-3., 1.5]
 
 
 class BodyInputs:
-    geometry_name = 'hemisphere_cylinder_shell'
-    sphere_radius = 0.25
-    cylinder_length = 1.0
-    arc_point_count = 11
-    line_point_count = 11
+    geometry_name = 'hyper_shell'
+    sphere_radius = 1.
     reference_length = sphere_radius
 
 
@@ -24,27 +21,15 @@ class EnvironmentInputs:
 class PDEInputs:
     exe_path = '/mnt/c/Users/Alexander/UbuntuShared/dimice-heat-dealii/bin/heat_problem'
     working_dir = 'C:\\Users\\Alexander\\UbuntuShared\\run\\'
-    semi_implicit_theta = 1.0
-    end_time = 0.01
-    time_step = 0.002
+    semi_implicit_theta = 0.7
+    end_time = 0.02
+    time_step = 0.001
     max_cells = 1000
-    hot = 1.0
-    warm = 0.1
-    cold = -1.0
-    dirichlet_boundary_ids = [5, 7, 9]
-    dirichlet_boundary_values = [hot, warm, hot]
-    neumann_boundary_ids = [0, 1, 2, 3, 4]
-    neumann_boundary_values = [cold, cold, cold, cold, cold]
-    dirichlet_ramp_boundary_ids = [6, 8]
-    dirichlet_ramp_start_values = [hot, hot]
-    dirichlet_ramp_end_values = [warm, warm]
+    dirichlet_boundary_ids = [1]
+    dirichlet_boundary_values = [-1.]
+    neumann_boundary_ids = [0]
+    neumann_boundary_values = [10.]
     def __init__(self, body):
         R = body.input.sphere_radius
-        L = body.input.cylinder_length
-        self.sizes = [R, 4*R, L, 2*L]
-        # @todo: Generalize handling of Dirichlet ramp boundaries
-        self.dirichlet_ramp_start_points = [R, 0.,
-                                           -R, 0.]
-        self.dirichlet_ramp_end_points = [R, L,
-                                         -R, L]
+        self.sizes = [R, 2*R]
 
