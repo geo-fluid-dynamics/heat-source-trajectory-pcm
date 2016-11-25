3b0e7d82091d95fb9eb8d0ed4d600871613eee4e
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2016-11-25 10:58:42 +0100

    Updated this example to work with the last commit, which made some big changes

--- examples/step_heat.py
+++ examples/step_heat.py
@@ -2,59 +2,65 @@
 import trajectory
 import pandas
 
-def make_time_history_row(trajectory):
-        return pandas.DataFrame(
-            {'step': trajectory.step, 
-             'time': trajectory.time,
-             'velocity': trajectory.state.velocity[1],
-             'depth': trajectory.state.get_position()[1],
-             'pde_depth': trajectory.pde.state.get_position()[1],
-             'heat_flux': trajectory.pde.input.bc.function_double_arguments[0]},
-            index=[trajectory.step])
-
-            
-def write_time_history(self):
-        self.time_history.to_csv(self.input.name+'/time_history.csv')
+def make_time_history_row(traj):
+    return pandas.DataFrame(
+        {'step': traj.step, 'time': traj.time,
+         'velocity': traj.state_dot.get_position()[1],
+         'depth': traj.state.get_position()[1],
+         'pde_depth': traj.pde.state.get_position()[1],
+         'heat_flux': traj.pde.bc.function_double_arguments[0]},
+        index=[traj.step])
         
         
 t = trajectory.Trajectory()
     
-t.input.name = 'step_heat'
+t.name = 'step_heat'
 
-t.input.step_count = 10
-t.input.time_step_size = 1.
+t.time_step_size = 1.
 
 r = 1.e-2
-t.body.input.sphere_radius = r
+t.body.sizes[0] = r
 
-t.pde.input.geometry.dim = 2
-t.pde.input.geometry.grid_name = 'hyper_shell'
-t.pde.input.geometry.sizes = [r, 2*r]
+t.pde.geometry.dim = 2
+t.pde.geometry.grid_name = 'hyper_shell'
+t.pde.geometry.sizes = [r, 2*r]
 
-t.pde.input.use_physical_diffusivity = True
+t.pde.use_physical_diffusivity = True
 
-t.pde.input.bc.implementation_types = ['natural', 'strong']
-t.pde.input.bc.function_names = ['constant', 'constant']
-t.pde.input.bc.function_double_arguments = [2.e-3, -1.]
+t.pde.bc.implementation_types = ['natural', 'strong']
+t.pde.bc.function_names = ['constant', 'constant']
+t.pde.bc.function_double_arguments = [2.e-3, -1.]
 
-t.pde.input.iv.function_name = 'constant'
-t.pde.input.iv.function_double_arguments = -1.
+t.pde.iv.function_name = 'constant'
+t.pde.iv.function_double_arguments = -1.
 
-t.pde.input.refinement.boundaries_to_refine = 0
-t.pde.input.refinement.initial_boundary_cycles = 3
-t.pde.input.refinement.initial_global_cycles = 2
+t.pde.refinement.boundaries_to_refine = 0
+t.pde.refinement.initial_boundary_cycles = 3
+t.pde.refinement.initial_global_cycles = 2
 
-t.pde.input.time.semi_implicit_theta = 1.
-t.pde.input.time.step_size = 0.2
+t.pde.time.semi_implicit_theta = 1.
+t.pde.time.step_size = 0.2
 
-t.pde.input.solver.tolerance = 1.e-8
-t.pde.input.solver.normalize_tolerance = False
+t.pde.solver.tolerance = 1.e-8
+t.pde.solver.normalize_tolerance = False
 
-t.run()
+time_history =  make_time_history_row(t)
+print(time_history)
 
-t.input.step_count = 20
-t.pde.input.bc.function_double_arguments[0] = 1.2*t.pde.input.bc.function_double_arguments[0]
-t.run()
+print("Running trajectory \'"+t.name+'\'')
 
-print(t.time_history)
-t.write_time_history()
+for step in range(0,10):
+    t.run_step()
+    time_history = time_history.append(make_time_history_row(t))
+    print(time_history)
+
+print("Increasing heat flux")
+t.pde.bc.function_double_arguments[0] = 1.2*t.pde.bc.function_double_arguments[0]
+
+for step in range(10, 20):
+    t.run_step()
+    time_history = time_history.append(make_time_history_row(t))
+    print(time_history)
+
+time_history.to_csv(t.name+'/time_history.csv')
+
